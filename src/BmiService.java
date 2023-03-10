610e218cc584707493cb5c5dcd19b5e3e135c54c
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-03-10 15:06:04 +0400

    First commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+        public double calculate (double weight, double height) {
+            double mass = (weight / (height*height));
+            return mass;
+        }
+    }
